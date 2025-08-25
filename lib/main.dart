2f23893ad9f3fee1157c83689858bc4cc0ec3577
Maksaline <msajib221134@bscse.uiu.ac.bd>
2025-08-26 04:18:44 +0600

    Fullscreen, App title, App Icon...

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,29 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:minimalist_api_tester/cubits/response_cubit.dart';
 import 'package:minimalist_api_tester/cubits/theme_cubit.dart';
 import 'package:minimalist_api_tester/pages/homepage/homepage.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = const WindowOptions(
+    title: 'Postboy Desktop',
+    size: Size(1920, 1080), // Set your preferred size
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.normal,
+  );
+
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.setTitle('Postboy Desktop');
+    await windowManager.show();
+    await windowManager.focus();
+    await windowManager.maximize();
+  });
+  
   runApp(BlocProvider(
       create: (context) => ThemeCubit(),
       child: const MyApp()
