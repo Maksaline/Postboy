8af1ff3cce4c549a8a820197558c3f3165c0d4b9
Maksaline <msajib221134@bscse.uiu.ac.bd>
2025-08-26 03:50:41 +0600

    All in try catch...

--- lib/cubits/response_cubit.dart
+++ lib/cubits/response_cubit.dart
@@ -32,191 +32,203 @@ class ResponseCubit extends Cubit<ResponseState> {
   }) async {
     emit(ResponseLoading());
 
-    final start = DateTime.now();
-    if (url.isEmpty) {
-      emit(ResponseFailure(message: "URL cannot be empty"));
-      return;
-    }
-
-    const JsonEncoder encoder = JsonEncoder.withIndent('    ');
-
-    Map<String, dynamic> requestHeaders = {};
-    if (authToken != null && authToken.isNotEmpty) {
-      requestHeaders['Authorization'] = 'Bearer $authToken';
-    }
-
-    Options options = Options(
-      headers: requestHeaders.isNotEmpty ? requestHeaders : null,
-    );
-
-    List<dynamic> responses = [];
-    dynamic newValue;
-    if(automationMap != null && automationMap.isNotEmpty && count != null && count > 0 && bodyMap != null && bodyMap.isNotEmpty) {
-      for(int i=0; i<count; i++) {
-        Map<String, dynamic> newRes = {};
-        Map<String, dynamic> newData = {};
-        bodyMap.forEach((key, value) {
-          if(bodyMap[key] != '<<<Automation>>>') {
-            newValue = bodyMap[key];
-          } else {
-            Map<String, dynamic> val = automationMap[key];
-            int lower = val['lower'] ?? 0;
-            int upper = val['upper'] ?? 0;
-            if (val['type'] == 'Int') {
-              if (val['option'] == 'Random') {
-                newValue = lower + rand.nextInt(upper - lower + 1);
-              } else {
-                newValue = min(lower + i, upper);
-              }
-            } else if (val['type'] == 'Double') {
-              double randomDouble = lower + rand.nextDouble() * (upper - lower);
-              if (val['option'] == 'Random') {
-                newValue = double.parse(randomDouble.toStringAsFixed(2));
-              } else {
-                newValue =
-                    double.parse(min(lower + i, upper).toStringAsFixed(2));
-              }
-            } else if (val['type'] == 'String') {
-              if (val['option'] == 'Random') {
-                int length = lower + rand.nextInt(upper - lower + 1);
-                newValue = NameGenerator.generateRandomName(length);
-              } else {
-                int length = min(lower + i, upper);
-                if (length > 20) {
-                  length = 20;
-                } else if (length < 1) {
-                  length = 1;
-                }
-                newValue = NameGenerator.generateRandomName(length);
-              }
-            } else if (val['type'] == 'Boolean') {
-              newValue =
-              val['option'] == 'Random' ? rand.nextBool() : (i % 2 == 0);
-            }
-          }
-          newData[key] = newValue;
-          newRes['${i+1}-$key'] = newValue;
-        });
-        // print(i+1);
-        // print(newData);
-        try{
-          if(method == 'GET') {
-            response = await dio.get(url, data: newData, options: options);
-          } else if(method == 'POST') {
-            response = await dio.post(url, data: newData, options: options);
-          } else if(method == 'PUT') {
-            response = await dio.put(url, data: newData, options: options);
-          } else if(method == 'DELETE') {
-            response = await dio.delete(url, data: newData, options: options);
-          } else {
-            response = await dio.patch(url, data: newData, options: options);
-          }
-          
-          newRes['${i+1}-StatusCode'] = '${response.statusCode} ${response.statusMessage}' ?? 0;
-          responses.add(newRes);
-          
-        }catch (e) {
-          emit(ResponseFailure(message: e.toString()));
-          return;
-        }
+    try {
+      final start = DateTime.now();
+      if (url.isEmpty) {
+        emit(ResponseFailure(message: "URL cannot be empty"));
+        return;
       }
-      final end = DateTime.now();
-      String autoBody = encoder.convert(responses);
 
-      emit(ResponseLoaded(
-        statusCode: 0,
-        statusMessage: 'Automation Completed',
-        headers: '',
-        body: autoBody,
-        time: end.difference(start).inMilliseconds,
-        size: autoBody.toString().length,
-      ));
-    } else {
-      try {
-        if (method == 'GET') {
-          response = await dio.get(url, data: bodyMap, options: options);
-        } else if (method == 'POST') {
-          response = await dio.post(url, data: bodyMap, options: options);
-        } else if (method == 'PUT') {
-          response = await dio.put(url, data: bodyMap, options: options);
-        } else if (method == 'DELETE') {
-          response = await dio.delete(url, data: bodyMap, options: options);
-        } else {
-          response = await dio.patch(url, data: bodyMap, options: options);
+      const JsonEncoder encoder = JsonEncoder.withIndent('    ');
+
+      Map<String, dynamic> requestHeaders = {};
+      if (authToken != null && authToken.isNotEmpty) {
+        requestHeaders['Authorization'] = 'Bearer $authToken';
+      }
+
+      Options options = Options(
+        headers: requestHeaders.isNotEmpty ? requestHeaders : null,
+      );
+
+      List<dynamic> responses = [];
+      dynamic newValue;
+      if (automationMap != null && automationMap.isNotEmpty && count != null &&
+          count > 0 && bodyMap != null && bodyMap.isNotEmpty) {
+        for (int i = 0; i < count; i++) {
+          Map<String, dynamic> newRes = {};
+          Map<String, dynamic> newData = {};
+          bodyMap.forEach((key, value) {
+            if (bodyMap[key] != '<<<Automation>>>') {
+              newValue = bodyMap[key];
+            } else {
+              Map<String, dynamic> val = automationMap[key];
+              int lower = val['lower'] ?? 0;
+              int upper = val['upper'] ?? 0;
+              if (val['type'] == 'Int') {
+                if (val['option'] == 'Random') {
+                  newValue = lower + rand.nextInt(upper - lower + 1);
+                } else {
+                  newValue = min(lower + i, upper);
+                }
+              } else if (val['type'] == 'Double') {
+                double randomDouble = lower +
+                    rand.nextDouble() * (upper - lower);
+                if (val['option'] == 'Random') {
+                  newValue = double.parse(randomDouble.toStringAsFixed(2));
+                } else {
+                  newValue =
+                      double.parse(min(lower + i, upper).toStringAsFixed(2));
+                }
+              } else if (val['type'] == 'String') {
+                if (val['option'] == 'Random') {
+                  int length = lower + rand.nextInt(upper - lower + 1);
+                  newValue = NameGenerator.generateRandomName(length);
+                } else {
+                  int length = min(lower + i, upper);
+                  if (length > 20) {
+                    length = 20;
+                  } else if (length < 1) {
+                    length = 1;
+                  }
+                  newValue = NameGenerator.generateRandomName(length);
+                }
+              } else if (val['type'] == 'Boolean') {
+                newValue =
+                val['option'] == 'Random' ? rand.nextBool() : (i % 2 == 0);
+              }
+            }
+            newData[key] = newValue;
+            newRes['${i + 1}-$key'] = newValue;
+          });
+          // print(i+1);
+          // print(newData);
+          try {
+            if (method == 'GET') {
+              response = await dio.get(url, data: newData, options: options);
+            } else if (method == 'POST') {
+              response = await dio.post(url, data: newData, options: options);
+            } else if (method == 'PUT') {
+              response = await dio.put(url, data: newData, options: options);
+            } else if (method == 'DELETE') {
+              response = await dio.delete(url, data: newData, options: options);
+            } else {
+              response = await dio.patch(url, data: newData, options: options);
+            }
+
+            newRes['${i + 1}-StatusCode'] =
+                '${response.statusCode} ${response.statusMessage}' ?? 0;
+            responses.add(newRes);
+          } catch (e) {
+            emit(ResponseFailure(message: e.toString()));
+            return;
+          }
         }
-
         final end = DateTime.now();
+        String autoBody = encoder.convert(responses);
 
-        int verdict = 0;
-        Map<int, int> keysChecked = {};
+        emit(ResponseLoaded(
+          statusCode: 0,
+          statusMessage: 'Automation Completed',
+          headers: '',
+          body: autoBody,
+          time: end
+              .difference(start)
+              .inMilliseconds,
+          size: autoBody
+              .toString()
+              .length,
+        ));
+      } else {
+        try {
+          if (method == 'GET') {
+            response = await dio.get(url, data: bodyMap, options: options);
+          } else if (method == 'POST') {
+            response = await dio.post(url, data: bodyMap, options: options);
+          } else if (method == 'PUT') {
+            response = await dio.put(url, data: bodyMap, options: options);
+          } else if (method == 'DELETE') {
+            response = await dio.delete(url, data: bodyMap, options: options);
+          } else {
+            response = await dio.patch(url, data: bodyMap, options: options);
+          }
 
-        if (expectedMap != null && expectedMap.isNotEmpty) {
-          for (var map in expectedMap.entries) {
-            int i = 0;
-            if (response.data is List) {
-              for (i = 0; i < response.data.length; i++) {
-                if (response.data[i] != null &&
-                    response.data[i][map.key] != null) {
+          final end = DateTime.now();
+
+          int verdict = 0;
+          Map<int, int> keysChecked = {};
+
+          if (expectedMap != null && expectedMap.isNotEmpty) {
+            for (var map in expectedMap.entries) {
+              int i = 0;
+              if (response.data is List) {
+                for (i = 0; i < response.data.length; i++) {
+                  if (response.data[i] != null &&
+                      response.data[i][map.key] != null) {
+                    if (map.value == "<<<Anything>>>" ||
+                        response.data[i][map.key].toString() ==
+                            map.value.toString()) {
+                      keysChecked[i] =
+                      keysChecked[i] != null ? keysChecked[i]! + 1 : 1;
+                    }
+                  }
+                }
+              } else {
+                if (response.data != null && response.data[map.key] != null) {
                   if (map.value == "<<<Anything>>>" ||
-                      response.data[i][map.key].toString() ==
+                      response.data[map.key].toString() ==
                           map.value.toString()) {
                     keysChecked[i] =
                     keysChecked[i] != null ? keysChecked[i]! + 1 : 1;
                   }
                 }
               }
-            } else {
-              if (response.data != null && response.data[map.key] != null) {
-                if (map.value == "<<<Anything>>>" ||
-                    response.data[map.key].toString() == map.value.toString()) {
-                  keysChecked[i] =
-                  keysChecked[i] != null ? keysChecked[i]! + 1 : 1;
-                }
-              }
             }
           }
-        }
-        int max = 0;
-        keysChecked.forEach((key, value) {
-          if (value > max) {
-            max = value;
+          int max = 0;
+          keysChecked.forEach((key, value) {
+            if (value > max) {
+              max = value;
+            }
+          });
+          if (max == expectedMap?.length) {
+            verdict = 1;
+          } else {
+            verdict = 2;
           }
-        });
-        if (max == expectedMap?.length) {
-          verdict = 1;
-        } else {
-          verdict = 2;
+          if (expectedMap == null || expectedMap.isEmpty) {
+            verdict = 0;
+          }
+
+
+          String formattedBody = encoder.convert(response.data);
+
+          String expectedString = '';
+          if (expectedMap != null && expectedMap.isNotEmpty) {
+            expectedString = encoder.convert(expectedMap);
+          }
+
+          emit(ResponseLoaded(
+            statusCode: response.statusCode ?? 0,
+            statusMessage: response.statusMessage ?? '',
+            headers: response.headers.map.toString(),
+            body: formattedBody,
+            time: end
+                .difference(start)
+                .inMilliseconds,
+            size: response.data
+                ?.toString()
+                .length ?? 0,
+            verdict: verdict,
+            expected: expectedString,
+          ));
+        } catch (e) {
+          emit(ResponseFailure(message: e.toString()));
+          return;
         }
-        if (expectedMap == null || expectedMap.isEmpty) {
-          verdict = 0;
-        }
-
-
-        String formattedBody = encoder.convert(response.data);
-
-        String expectedString = '';
-        if (expectedMap != null && expectedMap.isNotEmpty) {
-          expectedString = encoder.convert(expectedMap);
-        }
-
-        emit(ResponseLoaded(
-          statusCode: response.statusCode ?? 0,
-          statusMessage: response.statusMessage ?? '',
-          headers: response.headers.map.toString(),
-          body: formattedBody,
-          time: end
-              .difference(start)
-              .inMilliseconds,
-          size: response.data
-              ?.toString()
-              .length ?? 0,
-          verdict: verdict,
-          expected: expectedString,
-        ));
-      } catch (e) {
-        emit(ResponseFailure(message: e.toString()));
-        return;
       }
+    } catch (e) {
+      emit(ResponseFailure(message: e.toString()));
+      return;
     }
   }
 }
