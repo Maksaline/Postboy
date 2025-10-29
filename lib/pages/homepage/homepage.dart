edeed6992472b76756402ea95016a222fcaa3a97
Maksaline <msajib221134@bscse.uiu.ac.bd>
2025-10-30 00:44:47 +0600

    Titlebar done...

--- lib/pages/homepage/homepage.dart
+++ lib/pages/homepage/homepage.dart
@@ -4,6 +4,7 @@ import 'package:minimalist_api_tester/pages/homepage/widgets/builder_container.d
 import 'package:minimalist_api_tester/pages/homepage/widgets/collection_container.dart';
 import 'package:minimalist_api_tester/pages/homepage/widgets/response_container.dart';
 import 'package:minimalist_api_tester/pages/homepage/widgets/window_buttons.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -12,7 +13,35 @@ class Homepage extends StatefulWidget {
   State<Homepage> createState() => _HomepageState();
 }
 
-class _HomepageState extends State<Homepage> {
+class _HomepageState extends State<Homepage> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  bool _isExpanded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _toggleMenu() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+      if (_isExpanded) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,6 +66,66 @@ class _HomepageState extends State<Homepage> {
                         mainAxisAlignment: MainAxisAlignment.start,
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
+                          SizedBox(
+                            width: 150,
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                  onPressed: _toggleMenu,
+                                  icon: AnimatedIcon(
+                                    icon: AnimatedIcons.menu_close,
+                                    progress: _controller,
+                                    color: Theme.of(context).colorScheme.onSecondary,
+                                  ),
+                                  iconSize: 16,
+                                ),
+                                AnimatedSize(
+                                  duration: const Duration(milliseconds: 300),
+                                  curve: Curves.easeInOut,
+                                  child: SizedBox(
+                                    width: _isExpanded ? null : 0,
+                                    child: AnimatedOpacity(
+                                      opacity: _isExpanded ? 1.0 : 0.0,
+                                      duration: const Duration(milliseconds: 200),
+                                      child: TextButton(
+                                        onPressed: () async {
+                                          final Uri url = Uri.parse('https://github.com/Maksaline/Postboy');
+                                          if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
+                                          throw Exception('Could not launch $url');
+                                          }
+                                        },
+                                        style: TextButton.styleFrom(
+                                          foregroundColor: Theme.of(context).colorScheme.onSecondary,
+                                          shape : RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(0.0),
+                                          ),
+                                          padding: EdgeInsets.symmetric(horizontal: 2.0),
+                                        ),
+                                        child: RichText(
+                                          text: TextSpan(
+                                            children: [
+                                              TextSpan(
+                                                text: 'C',
+                                                style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                                                  decoration: TextDecoration.underline,
+                                                ),
+                                              ),
+                                              TextSpan(
+                                                text: 'ontribute',
+                                                style: Theme.of(context).textTheme.bodySmall,
+                                              ),
+                                            ],
+                                          ),
+                                        )
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Spacer(),
                           Container(
                             width: 18,
                             height: 18,
@@ -56,6 +145,7 @@ class _HomepageState extends State<Homepage> {
                             'Postboy',
                             style: Theme.of(context).textTheme.titleSmall
                           ),
+                          const Spacer(),
                         ],
                       ),
                     ),
